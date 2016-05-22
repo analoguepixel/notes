96eea54339d49eb971a4b5d6c360d7fea09f8848
Will Means <williammeans92@gmail.com>
2016-05-22 20:57:34 +0000

    added about page; made font size larger

--- about/index.php
+++ about/index.php
@@ -18,6 +18,20 @@
       <div class="page-body">
         <div class="row">
           <h1 class="center">About mininotes</h1>
+          <div class="center" style="width: 65%">
+            <p>
+              Mininotes is a personal project born out of dissatisfaction with popular note taking applications. It is designed to be fast and functional.
+            </p>
+            <h3>Main Goals of mininotes</h3>
+            <ul>
+              <li> <strong>Instant access</strong> - Allow a user to enter or view an note within three seconds of accessing the website/application.
+                <ul>
+                  <li><strong>Load quickly</strong> - The goal right now is that pages are small and load quicky. No page should exceed a 512kB download size.</li>
+                  <li><strong>Operate quickly</strong> - Do not make the user wait more than needed.</li>
+                </ul>
+              </li>
+              <li><strong>Scalable</strong> - Performance without pressure means nothing.</li>
+              <li><strong>Support many platforms</strong> - Everyone deserves a chance to record their thoughts. Supporting the web first means more users can access the platform from the start.</li>
       </div>
     </div>
 </body>
